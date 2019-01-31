fee872dd6b7b44c12daff57d15cc7376fd553b63
Tim Cho <timcho@Microsoft-XP.local>
2019-01-31 15:27:18 -0800

    added the extra operators to be worked on later. Made little fixes so the non-template codes run

--- assignment.cpp
+++ assignment.cpp
@@ -1,8 +1,8 @@
 #include <iostream>   // std::ostream, std::cout
 
-namespace Pic10b{
+namespace Pic10b {
     template<typename T>
-    class vector{
+    class vector {
     private:
         T* the_data;
         size_t the_size;
@@ -14,9 +14,19 @@ namespace Pic10b{
     public:
         // The big 4
         vector();
-        vector( const vector& );
-        vector& operator=( const vector& );
+        vector(const vector&);
+        vector& operator=(const vector& vec);
         ~vector();
+        
+        // Overloaded Operators
+        //vector& operator*(const vector<T>&, const T& c);
+        vector& operator+=(const vector<T>&);
+        bool operator>(const vector<T>&);
+        bool operator<(const vector<T>&);
+        bool operator>=(const vector<T>&);
+        bool operator<=(const vector<T>&);
+        bool operator==(const vector<T>&);
+        bool operator!=(const vector<T>&);
         
         // Other members [public]
         bool empty() const;
@@ -24,22 +34,23 @@ namespace Pic10b{
         size_t capacity() const;
         T front() const;
         T back() const;
-        T at( size_t index ) const;
-        T& operator[]( size_t index );
-        T operator[]( size_t index ) const;
-        void dump_data_to( std::ostream& out ) const;
+        T at(size_t index) const;
+        T& operator[](size_t index);
+        T operator[](size_t index) const;
+        void dump_data_to(std::ostream& out) const;
         void dump_data() const;
-        void push_back( T new_value );
+        void push_back(T new_value);
         void pop_back();
+        
         
     private:
         //Other members [private]
-        void reserve( size_t new_capacity );
+        void reserve(size_t new_capacity);
         
     }; // end Pic10b::vector
     
     
-    /** ************************* THE BIG 4 ************************* **/
+    /** *********************** THE BIG 4 *********************** **/
     template<typename T>
     vector<T>::vector()
     : the_data(nullptr), the_size(0), the_capacity(INIT_CAP) {
@@ -47,20 +58,20 @@ namespace Pic10b{
         the_data = new T[the_capacity];
     }
     template<typename T>
-    vector<T>::vector( const vector& source )
+    vector<T>::vector(const vector<T>& source)
     : the_data(nullptr), the_size(source.the_size),
     the_capacity(source.the_capacity) {
         
         the_data = new T[the_capacity];
         
         // Deep copy of internal array
-        for ( int i = 0 ; i < the_size ; ++i ){
+        for (int i = 0; i < the_size; ++i) {
             the_data[i] = source.the_data[i];
         }
     }
     template<typename T>
-    vector<T>& vector::operator=( const vector& rhs ) {
-        if ( this != &rhs ) {     // Self-assignment?
+    vector<T>& vector<T>::operator=(const vector<T>& rhs) {
+        if (this != &rhs) {     // Self-assignment?
             // Release old memory and request more
             delete[] the_data;
             the_data = new T[rhs.the_capacity];
@@ -70,17 +81,19 @@ namespace Pic10b{
             the_capacity = rhs.the_capacity;
             
             // Deep copy internal array
-            for ( int i = 0 ; i < the_size ; ++i )
+            for (int i = 0; i < the_size; ++i)
                 the_data[i] = rhs.the_data[i];
         }
         return *this;
     }
     template<typename T>
-    vector<T>::~vector(){
+    vector<T>::~vector() {
         delete[] the_data;
     }
+    /** ********************* Overloaded Operators ********************* **/
+
     
-    /** *********************** OTHER MEMBERS *********************** **/
+    /** ********************* OTHER MEMBERS ********************* **/
     template<typename T>
     bool vector<T>::empty() const {
         return the_size == 0;
@@ -102,50 +115,50 @@ namespace Pic10b{
         return *(the_data + the_size - 1);
     }
     template<typename T>
-    T vector<T>::at( size_t index ) const {
-        if ( index < the_size )
+    T vector<T>::at(size_t index) const {
+        if (index < the_size)
             return the_data[index];
         return the_data[0];
     }
     template<typename T>
-    T& vector<T>::operator[]( size_t index ){
+    T& vector<T>::operator[](size_t index) {
         return the_data[index];
     }
     template<typename T>
-    T vector<T>::operator[]( size_t index ) const {
+    T vector<T>::operator[](size_t index) const {
         return the_data[index];
     }
     template<typename T>
-    void vector<T>::dump_data_to( std::ostream& out ) const {
+    void vector<T>::dump_data_to(std::ostream& out) const {
         out << "Vector (dump): ";
-        for ( size_t i = 0 ; i < the_capacity ; ++i )
+        for (size_t i = 0; i < the_capacity; ++i)
             out << the_data[i] << ' ';
         out << '\n';
     }
     template<typename T>
     void vector<T>::dump_data() const {
-        dump_data_to( std::cout );
+        dump_data_to(std::cout);
     }
     
     template<typename T>
-    void vector<T>::push_back( double new_value ){
-        if ( the_size == the_capacity )
-            reserve( the_capacity + 1 );     // `the_data` is reassigned
+    void vector<T>::push_back(T new_value) {
+        if (the_size == the_capacity)
+            reserve(the_capacity + 1);     // the_data is reassigned
         
         the_data[the_size++] = new_value;
     }
     
     // This implementation does not shrink the vector (ever)
     template<typename T>
-    void vector<T>::pop_back(){
-        if ( the_size > 0 )
+    void vector<T>::pop_back() {
+        if (the_size > 0)
             --the_size;
     }
     
     template<typename T>
-    void vector<T>::reserve( size_t new_capacity ){
-        if ( new_capacity > the_capacity ) {
-            if ( new_capacity <= 2 * the_capacity )
+    void vector<T>::reserve(size_t new_capacity) {
+        if (new_capacity > the_capacity) {
+            if (new_capacity <= 2 * the_capacity)
                 new_capacity = 2 * the_capacity;
             
             T* old_location = the_data;
@@ -153,7 +166,7 @@ namespace Pic10b{
             the_data = new T[new_capacity];
             the_capacity = new_capacity;
             
-            for ( size_t i = 0 ; i < the_size ; ++i )
+            for (size_t i = 0; i < the_size; ++i)
                 the_data[i] = old_location[i];
             
             delete old_location;
@@ -164,32 +177,30 @@ namespace Pic10b{
 
 
 
-/** ************************ OTHER FUNCTIONS ************************ **/
+/** ********************** OTHER FUNCTIONS ********************** **/
 template<typename T>
-std::ostream& operator<<( std::ostream& out, const Pic10b::vector<T>& v ){
-    for ( size_t i = 0 ; i < v.size() ; ++i )
+std::ostream& operator<<(std::ostream& out, const Pic10b::vector<T>& v) {
+    for (size_t i = 0; i < v.size(); ++i)
         out << v[i] << ' ';
     return out;
 }
 
 template<typename T>
-void print_vector( const Pic10b::vector<T>& v ){
-    if ( v.empty() )
+void print_vector(const Pic10b::vector<T>& v) {
+    if (v.empty())
         std::cout << "Vector is empty\n";
     else
-        std::cout << "Vector (contents): " << v << '\n' ;
+        std::cout << "Vector (contents): " << v << '\n';
 }
-
 
 
 /** ************************* THE DRIVER ************************ **/
 int main(){
-    
     using Pic10b::vector;
     using std::cout;
     
     cout << "Create & display empty vector (v1)\n";
-    vector v1;
+    vector<int> v1;
     print_vector(v1);
     v1.dump_data();
     
@@ -200,7 +211,7 @@ int main(){
     print_vector(v1);
     
     cout << "\nCopy non-empty vector, pop back last entry & display (v2)\n";
-    vector v2(v1);
+    vector<int> v2(v1);
     v2.pop_back();
     print_vector(v2);
     
